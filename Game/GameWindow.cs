f123dfa3ec7bce091c578a71edb35df1955129e7
az-inc <developer@Alexs-MacBook-Pro.local>
2014-05-10 13:43:04 -0400

    Options
    Updated the options buttons so they output things

--- Game/GameWindow.cs
+++ Game/GameWindow.cs
@@ -84,50 +84,119 @@ public partial class GameWindow: Gtk.Window
 
 		option = mainr.Next(1, 11);
 		if (option == 1) {
-			r2 = "Run";
+			r3 = "Run";
 		}
 		if (option == 2) {
-			r2 = "Jump";
+			r3 = "Fly";
 		}
 		if (option == 3) {
-			r2 = " Hat";
+			r3 = "Eat Melon Rines";
 		}
 		if (option == 4) {
-			r2 = " Phone";
+			r3 = "Attack somthing";
 		}
 		if (option == 5) {
-			r2 = " Gun";
+			r3 = "Look for and press all buttons";
 		}
 		if (option == 6) {
-			r2 = " Snowman";
+			r3 = "Sit Down";
 		}
 		if (option == 7) {
-			r2 = " Ranbow Maker";
+			r3 = "Steal some gold coins";
 		}
 		if (option == 8) {
-			r2 = " Google Glass... Oh wait nvm i dont have those";
+			r3 = "use a pair Google Glass... Oh wait they are 1000$";
 		}
 		if (option == 9) {
-			r2 = " Helmet";
+			r3 = "Sit around and do nothing";
 		}
 		if (option == 10) {
-			r2 = " Cat";
+			r3 = "Order somthing off ebay";
 		}
 
+		option = mainr.Next(1, 11);
+		if (option == 1) {
+			r4 = "Run";
+		}
+		if (option == 2) {
+			r4 = "Fly";
+		}
+		if (option == 3) {
+			r4 = "Eat Melon Rines";
+		}
+		if (option == 4) {
+			r4 = "Attack somthing";
+		}
+		if (option == 5) {
+			r4 = "Look for and press all buttons";
+		}
+		if (option == 6) {
+			r4 = "Sit Down";
+		}
+		if (option == 7) {
+			r4 = "Steal some gold coins";
+		}
+		if (option == 8) {
+			r4 = "use a pair Google Glass... Oh wait they are 1000$";
+		}
+		if (option == 9) {
+			r4 = "Sit around and do nothing";
+		}
+		if (option == 10) {
+			r4 = "Order somthing off ebay";
+		}
+
+		option = mainr.Next(1, 11);
+		if (option == 1) {
+			r5 = "Run";
+		}
+		if (option == 2) {
+			r5 = "Fly";
+		}
+		if (option == 3) {
+			r5 = "Eat Melon Rines";
+		}
+		if (option == 4) {
+			r5 = "Attack somthing";
+		}
+		if (option == 5) {
+			r5 = "Look for and press all buttons";
+		}
+		if (option == 6) {
+			r5 = "Sit Down";
+		}
+		if (option == 7) {
+			r5 = "Steal some gold coins";
+		}
+		if (option == 8) {
+			r5 = "use a pair Google Glass... Oh wait they are 1000$";
+		}
+		if (option == 9) {
+			r5 = "Sit around and do nothing";
+		}
+		if (option == 10) {
+			r5 = "Order somthing off ebay";
+		}
 	}
 
 
 	protected void OnO1Clicked (object sender, EventArgs e)
 	{
-		txt.LabelProp = "I found myself in a "+ place + " with my trusty " + r2 + "";
-		o1.Label = "blank";
-		o2.Label = "blank";
-		o3.Label = "blank";
-		o4.Label = "blank";
+		txt.LabelProp = "I found myself in a "+ place + " with my trusty " + r2 + ". I decided to...";
+		o1.Label = r3;
+		o2.Label = r4;
+		o3.Label = "use " + r2;
+		o4.Label = r5;
 	}
 	public readonly string place = "loading";
 
 	public readonly string r2 = "loading";
+
+	public readonly string r3 = "loading";
+
+	public readonly string r4 = "loading";
+
+	public readonly string r5 = "loading";
 
 
 
